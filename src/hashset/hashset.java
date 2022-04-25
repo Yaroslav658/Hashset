4cb88ff0196fde8fb254ba8612bd2b3cae1eeee2
Yaroslav658 <101650091+Yaroslav658@users.noreply.github.com>
2022-04-25 18:48:02 +0300

    commit

--- src/hashset/hashset.java
+++ src/hashset/hashset.java
@@ -7,21 +7,19 @@ public class hashset {
 //Расписать понимание хешкода, начиная с класса Object.
 
 // if (a==null) return 0;
+//        Set<Object> set = new HashSet<>();
 
 
     public static void main(String[] args) {
 //        Set<Object> set = new HashSet<>();
         Object object = new Object();
+        System.out.println(object.hashCode());
         Object object2 = new Object();
-        Object object3 = new Object();
+        System.out.println(object2.hashCode());
+        Object object3;
         object3 = object2;
-        int set;
-        int set2;
-        int set3;
-        set = object.hashCode();
-        set2 = object2.hashCode();
-        set3 = object3.hashCode();
-        System.out.println(set+"\n"+set2+"\n"+set3);
+        System.out.println(object3.hashCode());
+
 
 
 
